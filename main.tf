abf1b6e11e309126cc2ed94f2299a73bcfeaab16
Hamouda <juniorhamouda7@gmail.com>
2025-06-17 13:49:06 +0100

    Tester automatisation

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
-   tags = {
-    environment = "verification"
-  }
+  tags = {
+
+  test       = "automatisation"
+}
 }
 
 resource "azurerm_log_analytics_workspace" "monitoring" {
