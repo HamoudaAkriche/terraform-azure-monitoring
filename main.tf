d20f5faf83f706cff5f3abc5e1dbb43b3fe9d6ce
Hamouda <juniorhamouda7@gmail.com>
2025-06-21 12:09:45 +0100

    Ajouter SSH + NSG

--- main.tf
+++ main.tf
@@ -62,8 +62,13 @@ resource "azurerm_linux_virtual_machine" "vm_monitoring" {
   resource_group_name = azurerm_resource_group.main.name
   size                = "Standard_B1s"
   admin_username      = var.admin_username
-  admin_password      = var.admin_password
-  disable_password_authentication = false
+  disable_password_authentication = true
+
+admin_ssh_key {
+  username   = var.admin_username
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFbmRrvPF1wlpHIoJpaokAVA5co+U6PDLIaPXch97+QZN/i2Crbq6cbWOy074KVwP6cpCTrn6qSW/OCmuaWPqQxZlJYltgGnbr4hSChHaBcvBCFEF0B7Fv7RsUwJIuASVZtcZKcRVZ/4vRXEKfZliaB3triweeppxPP+W2VE8Gxq/jLmD1u3CGus0ypEeFCHBOA+/rolOb8JxHgHeQjiF17/I2Q6xQjfhvXLmSlRkYD9KgS0vG2OMujZeHv/+bIN6FxrgUd0Zj4OIYXDjC2YK5hOJ6I/jyRxq0QAwJd3JQZNs1ks6hNtlnm8h+nybV+ojeVf1ndOTcOGZfGQZoSsRF"
+}
+
    
 
   network_interface_ids = [
@@ -122,4 +127,27 @@ resource "azurerm_network_interface_security_group_association" "nic_nsg" {
   network_interface_id      = azurerm_network_interface.main_nic.id
   network_security_group_id = azurerm_network_security_group.web_nsg.id
 }
+
+resource "azurerm_network_security_group" "nsg_ssh" {
+  name                = "nsg-ssh"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.main_nic.id
+  network_security_group_id = azurerm_network_security_group.nsg_ssh.id
+}
+
 
